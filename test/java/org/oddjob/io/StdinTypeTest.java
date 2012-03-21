fd387894542016db2b9325eb3885a77717e16ba4
Rob Gordon <rob@rgordon.co.uk>
2012-03-21 21:16:13 +0000

    Stop StdInType example creating a foo.txt file.

--- test/java/org/oddjob/io/StdinTypeTest.java
+++ test/java/org/oddjob/io/StdinTypeTest.java
@@ -1,5 +1,9 @@
 package org.oddjob.io;
 
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+
+import org.apache.commons.beanutils.PropertyUtils;
 import org.oddjob.FragmentHelper;
 import org.oddjob.arooa.ArooaParseException;
 
@@ -7,13 +11,15 @@ import junit.framework.TestCase;
 
 public class StdinTypeTest extends TestCase {
 
-	public void testExample() throws ArooaParseException {
+	public void testExample() throws ArooaParseException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
 		
 		// Just test XML for now.
 
 		FragmentHelper helper = new FragmentHelper();
-		helper.createComponentFromResource(
+		Object copy = helper.createComponentFromResource(
 				"org/oddjob/io/StdinTypeExample.xml");
-		
+				
+		assertEquals(new File("foo.txt"), 
+				PropertyUtils.getProperty(copy, "to"));
 	}
 }
