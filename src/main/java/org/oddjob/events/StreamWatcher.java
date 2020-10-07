55766cadaa9f849287d12f3ae706c4906e5ca1a8
Rob <rob@rgordon.co.uk>
2020-10-07 13:49:59 +0100

    Add Stream Watcher Event Source.

---
+++ src/main/java/org/oddjob/events/StreamWatcher.java
@@ -0,0 +1,82 @@
+package org.oddjob.events;
+
+import org.oddjob.util.Restore;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+
+/**
+ * @oddjob.description Watch a stream for an instance of a string. Typically this watcher (and its trigger)
+ * will be started before a job that produces some output that needs to be monitored.
+ * <p>
+ *     A typeical use case would be running an exec job that tails a log file, and this watcher is looking
+ *     for the word "Error" or some such.
+ * </p>
+ */
+public class StreamWatcher extends EventSourceBase<String> {
+
+    /**
+     * @oddjob.property
+     * @oddjob.description The text to watch for.
+     * @oddjob.required Yes.
+     */
+    private String watch;
+
+    /**
+     * @oddjob.property
+     * @oddjob.description The output stream to be provided to something who's output need watching.
+     */
+    private OutputStream out;
+
+    @Override
+    protected Restore doStart(Consumer<? super EventOf<String>> consumer) throws Exception {
+
+         AtomicReference<Consumer<? super EventOf<String>>> consumerRef =
+                 new AtomicReference<>(consumer);
+
+
+        final byte[] watch = Optional.ofNullable(this.watch)
+                .map(String::getBytes)
+                .orElseThrow(() -> new IllegalArgumentException("Nothing to watch"));
+
+        this.out = new OutputStream() {
+
+            int index = 0;
+            @Override
+            public void write(int b) throws IOException {
+                if ( b != watch[index++]) {
+                    index = 0;
+                }
+
+                if ( index == watch.length) {
+                    index = 0;
+                    Optional.ofNullable(consumerRef.get())
+                            .ifPresent(c -> c.accept(EventOf.of(new String(watch))));
+                }
+            }
+
+            @Override
+            public String toString() {
+                return "StreamWatcherOutputStream";
+            }
+        };
+
+        return () -> consumerRef.set(null);
+    }
+
+
+    public String getWatch() {
+        return watch;
+    }
+
+    public void setWatch(String watch) {
+        this.watch = watch;
+    }
+
+    public OutputStream getOut() {
+        return out;
+    }
+}
---
+++ src/test/java/org/oddjob/events/StreamWatcherTest.java
@@ -0,0 +1,48 @@
+package org.oddjob.events;
+
+import org.junit.Test;
+import org.oddjob.Oddjob;
+import org.oddjob.OddjobLookup;
+import org.oddjob.arooa.convert.ArooaConversionException;
+import org.oddjob.state.ParentState;
+import org.oddjob.tools.StateSteps;
+
+import java.io.File;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class StreamWatcherTest {
+
+    @Test
+    public void testExample() throws ArooaConversionException, InterruptedException {
+
+        Oddjob oddjob = new Oddjob();
+        oddjob.setFile(new File(getClass().getResource("StreamWatcherExample.xml").getFile()));
+
+        oddjob.run();
+
+        assertThat(oddjob.lastStateEvent().getState(), is(ParentState.ACTIVE));
+
+        OddjobLookup lookup = new OddjobLookup(oddjob);
+
+        StateSteps states = new StateSteps(oddjob);
+        states.startCheck(ParentState.ACTIVE, ParentState.COMPLETE);
+
+
+        Runnable job1 = lookup.lookup("job1", Runnable.class);
+        job1.run();
+
+        assertThat(oddjob.lastStateEvent().getState(), is(ParentState.ACTIVE));
+
+        Runnable job2 = lookup.lookup("job2", Runnable.class);
+        job2.run();
+
+        states.checkWait();
+
+        String text = lookup.lookup("echo.text", String.class);
+
+        assertThat(text, is("Found Apple"));
+    }
+
+}
