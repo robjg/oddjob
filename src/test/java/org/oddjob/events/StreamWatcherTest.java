04cf5f4433f745afabd4b1422160faf8b17a8996
Rob Gordon <rob@rgordon.co.uk>
2025-05-28 18:41:01 +0100

    Fix example Test. Improve Stream Watcher test.

--- src/test/java/org/oddjob/events/StreamWatcherTest.java
+++ src/test/java/org/oddjob/events/StreamWatcherTest.java
@@ -1,23 +1,36 @@
 package org.oddjob.events;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.oddjob.Oddjob;
 import org.oddjob.OddjobLookup;
 import org.oddjob.Stateful;
 import org.oddjob.arooa.convert.ArooaConversionException;
 import org.oddjob.state.ParentState;
 import org.oddjob.tools.StateSteps;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.net.URISyntaxException;
 import java.util.Objects;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class StreamWatcherTest {
+class StreamWatcherTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(StreamWatcherTest.class);
+
+    @BeforeEach
+    public void setUp(TestInfo testInfo) throws URISyntaxException, ArooaConversionException {
+
+        logger.info("---- starting {} -----", testInfo.getDisplayName());
+    }
 
     @Test
-    public void testExample() throws ArooaConversionException, InterruptedException {
+    void testExample() throws ArooaConversionException, InterruptedException {
 
         Oddjob oddjob = new Oddjob();
         oddjob.setFile(new File(Objects.requireNonNull(getClass()
@@ -47,6 +60,7 @@ public class StreamWatcherTest {
         Runnable job2 = lookup.lookup("job2", Runnable.class);
         job2.run();
 
+        // Why does this fail intermittently?
         states.checkWait();
 
         String text = lookup.lookup("echo.text", String.class);
