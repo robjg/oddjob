af8cfc492b7b6425defa9ac0517ca82c7794133b
Rob Gordon <rob@rgordon.co.uk>
2025-11-15 17:17:08 +0000

    ConnectionType no longer needs Driver Class.

--- src/main/java/org/oddjob/sql/ConnectionType.java
+++ src/main/java/org/oddjob/sql/ConnectionType.java
@@ -3,18 +3,19 @@
  */
 package org.oddjob.sql;
 
+import org.oddjob.arooa.convert.ArooaConversionException;
+import org.oddjob.arooa.types.ValueFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.io.Serial;
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.Driver;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
-
-import javax.inject.Inject;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.oddjob.arooa.convert.ArooaConversionException;
-import org.oddjob.arooa.types.ValueFactory;
 
 /**
  * @oddjob.description Definition for a Database connection.
@@ -26,14 +27,17 @@ import org.oddjob.arooa.types.ValueFactory;
  * @author Rob Gordon.
  */
 public class ConnectionType implements ValueFactory<Connection>, Serializable {
-	private final static long serialVersionUID = 20070315;
+
+	@Serial
+    private final static long serialVersionUID = 20070315;
 	
 	private static final Logger logger = LoggerFactory.getLogger(ConnectionType.class);
 		
 	/** 
 	 * @oddjob.property
-	 * @oddjob.description The driver class name.
-	 * @oddjob.required Yes. 
+	 * @oddjob.description The driver class name. Only required if using a custom classpath
+     * when the driver won't have registered itself using the standard ServiceLoader mechanism.
+	 * @oddjob.required No.
 	 */
 	private String driver;
 	
@@ -46,14 +50,14 @@ public class ConnectionType implements ValueFactory<Connection>, Serializable {
 	
 	/** 
 	 * @oddjob.property
-	 * @oddjob.description The database username..
+	 * @oddjob.description The database username.
 	 * @oddjob.required No. 
 	 */
 	private String username;
 
 	/** 
 	 * @oddjob.property
-	 * @oddjob.description The users password.
+	 * @oddjob.description The user's password.
 	 * @oddjob.required No. 
 	 */
 	private String password;
@@ -70,36 +74,22 @@ public class ConnectionType implements ValueFactory<Connection>, Serializable {
 	 * @see org.oddjob.arooa.types.ValueFactory#toValue()
 	 */
 	public Connection toValue() throws ArooaConversionException {
-		if (driver == null) {
-			throw new NullPointerException("Driver must be provided.");
-		}
 		if (url == null) {
 			throw new NullPointerException("Url must be provided.");
 		}
-		
-		ClassLoader loader = classLoader;
-		if (loader == null) {
-			loader = getClass().getClassLoader();
-		}
-		
-		Class<?> driverClass;
-		try {
-			// Attempting to fix Deadlock issues possibly caused by 
-			// http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6354348
-			synchronized (Driver.class) {
-				driverClass = Class.forName(driver, true, loader);
-			}
-		} catch (ClassNotFoundException e) {
-			throw new ArooaConversionException(e);					
-		}
 
-		Driver theDriver;
-		try {
-			theDriver = (Driver) driverClass.newInstance(); 
-		} catch (Exception e) {
-			throw new ArooaConversionException(e);					
-		}
-		
+        Driver theDriver;
+        if (this.driver == null) {
+            try {
+                theDriver = DriverManager.getDriver(url);
+            } catch (SQLException e) {
+                throw new ArooaConversionException("Failed to find a driver for url: " + url, e);
+            }
+        }
+        else {
+            theDriver = driverFromClass(driver);
+        }
+
 		Properties info = new Properties();
 		if (username != null) {
 		    info.put("user", username);
@@ -115,8 +105,8 @@ public class ConnectionType implements ValueFactory<Connection>, Serializable {
 						"No connection available for [" + url + "]");
 			}
 			return connection;
-		} catch (SQLException e) {					
-			logger.warn("Failed creating connection to: " + url, e);
+		} catch (SQLException e) {
+            logger.warn("Failed creating connection to: {}", url, e);
 			for (SQLException ce = e.getNextException(); ce != null; 
 					ce = ce.getNextException()) {
 				logger.warn("Next chained exception:", ce);						
@@ -125,6 +115,33 @@ public class ConnectionType implements ValueFactory<Connection>, Serializable {
 		}
 	}
 	
+    protected Driver driverFromClass(String driver) throws ArooaConversionException {
+
+        ClassLoader loader = classLoader;
+        if (loader == null) {
+            loader = getClass().getClassLoader();
+        }
+
+        Class<? extends Driver> driverClass;
+
+        try {
+            // Attempting to fix Deadlock issues possibly caused by
+            // http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6354348
+            synchronized (Driver.class) {
+                //noinspection unchecked
+                driverClass = (Class<? extends Driver>) Class.forName(driver, true, loader);
+            }
+        } catch (ClassNotFoundException e) {
+            throw new ArooaConversionException(e);
+        }
+
+        try {
+            return driverClass.getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            throw new ArooaConversionException(e);
+        }
+    }
+
 	/**
 	 * Get this connections driver class name.
 	 * 
--- src/test/java/org/oddjob/sql/ConnectionTypeTest.java
+++ src/test/java/org/oddjob/sql/ConnectionTypeTest.java
@@ -1,75 +1,108 @@
 package org.oddjob.sql;
 
-import org.junit.Test;
 
-import org.oddjob.OjTestCase;
-
+import org.junit.jupiter.api.Test;
 import org.oddjob.OddjobSessionFactory;
 import org.oddjob.arooa.ArooaBeanDescriptor;
-import org.oddjob.arooa.ArooaParseException;
 import org.oddjob.arooa.ArooaSession;
 import org.oddjob.arooa.convert.ArooaConversionException;
 import org.oddjob.arooa.life.SimpleArooaClass;
 import org.oddjob.tools.OddjobTestHelper;
 
-public class ConnectionTypeTest extends OjTestCase {
+import java.sql.Connection;
+import java.sql.SQLException;
 
-   @Test
-	public void testSerialize() throws Exception {
-		ConnectionType test = new ConnectionType();
-		test.setUrl("x:/y/z");
-		
-		ConnectionType copy = (ConnectionType) OddjobTestHelper.copy(test);
-		
-		assertEquals("x:/y/z", copy.getUrl());
-	}
-	
-   @Test
-	public void testIsClassLoaderAuto() throws ArooaParseException {
-		
-		ArooaSession session = new OddjobSessionFactory().createSession();
-		
-		ArooaBeanDescriptor descriptor = 
-			session.getArooaDescriptor().getBeanDescriptor(
-					new SimpleArooaClass(ConnectionType.class),
-					session.getTools().getPropertyAccessor());
-		
-		assertTrue(descriptor.isAuto("classLoader"));
-	}
-	
-   @Test
-	public void testBadUrl() {
-		
-		ConnectionType test = new ConnectionType();
-		test.setDriver("org.hsqldb.jdbcDriver");
-		test.setUrl("jdbc.url=jdbc:hsqldb:mem:test");
-		test.setUsername("sa");
-		test.setPassword("");
-		
-		try {
-			test.toValue();
-			fail("Should fail.");
-		} catch (ArooaConversionException e) {
-			assertTrue(e.getMessage().startsWith(
-					"No connection available for"));
-		}
-	}
-	
-   @Test
-	public void testBadUrl2() {
-		
-		ConnectionType test = new ConnectionType();
-		test.setDriver("org.hsqldb.jdbcDriver");
-		test.setUrl("jdbc.url=");
-		test.setUsername("sa");
-		test.setPassword("");
-		
-		try {
-			test.toValue();
-			fail("Should fail.");
-		} catch (ArooaConversionException e) {
-			assertTrue(e.getMessage().startsWith(
-					"No connection available for"));
-		}
-	}
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConnectionTypeTest {
+
+    @Test
+    void connectionWithDriver() throws SQLException {
+
+        ConnectionType test = new ConnectionType();
+        test.setDriver("org.hsqldb.jdbcDriver");
+        test.setUrl("jdbc:hsqldb:mem:test");
+        test.setUsername("sa");
+        test.setPassword("");
+
+        try (Connection connection = test.toValue()) {
+            assertFalse(connection.isClosed());
+        } catch (ArooaConversionException e) {
+            assertTrue(e.getMessage().startsWith(
+                    "No connection available for"));
+        }
+    }
+
+    @Test
+    void connectionWithoutDriver() throws SQLException {
+
+        ConnectionType test = new ConnectionType();
+        test.setUrl("jdbc:hsqldb:mem:test");
+        test.setUsername("sa");
+        test.setPassword("");
+
+        try (Connection connection = test.toValue()) {
+            assertFalse(connection.isClosed());
+        } catch (ArooaConversionException e) {
+            assertTrue(e.getMessage().startsWith(
+                    "No connection available for"));
+        }
+    }
+
+    @Test
+    public void testSerialize() throws Exception {
+        ConnectionType test = new ConnectionType();
+        test.setUrl("x:/y/z");
+
+        ConnectionType copy = OddjobTestHelper.copy(test);
+
+        assertEquals("x:/y/z", copy.getUrl());
+    }
+
+    @Test
+    public void testIsClassLoaderAuto() {
+
+        ArooaSession session = new OddjobSessionFactory().createSession();
+
+        ArooaBeanDescriptor descriptor =
+                session.getArooaDescriptor().getBeanDescriptor(
+                        new SimpleArooaClass(ConnectionType.class),
+                        session.getTools().getPropertyAccessor());
+
+        assertTrue(descriptor.isAuto("classLoader"));
+    }
+
+    @Test
+    public void testBadUrl() throws SQLException {
+
+        ConnectionType test = new ConnectionType();
+        test.setDriver("org.hsqldb.jdbcDriver");
+        test.setUrl("jdbc.url=jdbc:hsqldb:mem:test");
+        test.setUsername("sa");
+        test.setPassword("");
+
+        try (Connection ignored = test.toValue()) {
+            fail("Should fail.");
+        } catch (ArooaConversionException e) {
+            assertTrue(e.getMessage().startsWith(
+                    "No connection available for"));
+        }
+    }
+
+    @Test
+    public void testBadUrl2() throws SQLException {
+
+        ConnectionType test = new ConnectionType();
+        test.setDriver("org.hsqldb.jdbcDriver");
+        test.setUrl("jdbc.url=");
+        test.setUsername("sa");
+        test.setPassword("");
+
+        try (Connection ignored = test.toValue()) {
+            fail("Should fail.");
+        } catch (ArooaConversionException e) {
+            assertTrue(e.getMessage().startsWith(
+                    "No connection available for"));
+        }
+    }
 }
